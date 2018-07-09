48230990ede14dfcfaec0c7068fabbeb81a73deb
Shanmugapriya Ravindran <priya.ravindran009@gmail.com>
2018-07-09 15:01:39 -0500

    Quit chat implemented

--- client.c
+++ client.c
@@ -5,30 +5,45 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <signal.h>
+
+void sigintHandler(int sig_num)
+{
+   signal(SIGINT, sigintHandler);
+   printf("\nTerminating\n");
+   exit(0);
+}
 
 int main(int argc, char *argv[])
 {
+   signal(SIGINT,sigintHandler);    //To catch Ctr+C
+
    int sockFd, portNo;
    struct sockaddr_in serverAdd;
    int n;
-   struct hostent *server;
+   struct hostent *server;  //struct containing server details
+   int bye,byebye;
 
-   char buffer[256];
+   char txBuffer[256], rxBuffer[256];   //Rx Tx Buffers
 
    if(argc < 3) {
         printf("Argument Error\n");
         exit(1);
    }
-   portNo = atoi(argv[2]);
 
+   portNo = atoi(argv[2]);  //Fetch Port Number
+
+   //Creating a socket and fetching its file descriptor
    sockFd = socket(AF_INET, SOCK_STREAM, 0); //Arg 1: using IP Arg 2: using TCP Arg 3: provide appropriate socket
    if(sockFd < 0) {
         printf("Error Opening Socket\n");
+        exit(0);
    }
 
-    server = gethostbyname(argv[1]);
+   server = gethostbyname(argv[1]); //pass the name to get the address of the server
 
     if(server == NULL) {
         printf("No such host exists\n");
@@ -37,34 +52,53 @@ int main(int argc, char *argv[])
 
    bzero((char *) &serverAdd, sizeof(serverAdd));
 
-   serverAdd.sin_family = AF_INET;
-   bcopy((char *)server->h_addr,(char *)&serverAdd.sin_addr.s_addr,server->h_length);
+   serverAdd.sin_family = AF_INET;  //Internet type
+   bcopy((char *)server->h_addr,(char *)&serverAdd.sin_addr.s_addr,server->h_length);   //serverr addr
    serverAdd.sin_port = htons(portNo); //convert values between host and network 
 
+   //request connection to the server
    if(connect(sockFd, &serverAdd, sizeof(serverAdd)) < 0) {
         printf("Connection Error\n");
+        close(sockFd);
+        exit(0);
+   }
+   else {
+       printf("Connected to %s\n",inet_ntoa(serverAdd.sin_addr));
    }
 
-   printf("Enter the message\n");
 
-   while(1) {
-    bzero(buffer,255);
-    fgets(buffer,255,stdin);
-    n = write(sockFd,buffer,strlen(buffer));
+   do {
 
-    if(n < 0) {
+        printf("Enter message: ");
+        bzero(txBuffer,255);
+        fgets(txBuffer,255,stdin);
+        bye = strncasecmp(txBuffer,"bye",3);
+        n = send(sockFd,txBuffer,strlen(txBuffer),0);
+        if(n < 0) {
             printf("Error writing to socket\n");
-    }
-    bzero(buffer,256);
+            break;
+        }
 
-    n = read(sockFd,buffer,255);
-    
-    if(n < 0) {
+        if(bye == 0)
+            break;
+      
+        bzero(rxBuffer,256);
+        n = recv(sockFd,rxBuffer,255,0);
+        if(n < 0) {
             printf("Error reading from socket\n");
-    }
-    else {
-            printf("message: %s\n",buffer);    
-    }
-   }
+            break;
+        }
+        else {
+            printf("Received message: %s\n",rxBuffer);    
+        }
+        byebye =  strncasecmp(rxBuffer,"bye",3);
+        printf("byebye: %d\n",byebye);
+        if(byebye == 0)
+        break;
+
+   } while(1);
+   printf("Closing socket\n");
+   close(sockFd);
+
    return 0;
 }
--- server.c
+++ server.c
@@ -3,16 +3,28 @@
 #include <strings.h>
 #include <string.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <signal.h>
+
+void sigintHandler(int sig_num)
+{
+   signal(SIGINT, sigintHandler);
+   printf("\nTerminating\n");
+   exit(0);
+}
 
 int main(int argc, char *argv[])
 {
-   char buffer[256],txBuffer[256];
+   signal(SIGINT,sigintHandler);
+
+   char rxBuffer[256],txBuffer[256];
    int sockFd, clientSocketFd, portNo, clientLen;
    struct sockaddr_in serverAdd, clientAdd;
    int n;
+   int bye,byebye;
 
    if(argc < 2) {
         printf("Argument Error\n");
@@ -22,55 +34,72 @@ int main(int argc, char *argv[])
    sockFd = socket(AF_INET, SOCK_STREAM, 0); //Arg 1: using IP Arg 2: using TCP Arg 3: provide appropriate socket
    if(sockFd < 0) {
         printf("Error Opening Socket\n");
+        exit(0);
    }
-
-   bzero((char *) &serverAdd, sizeof(serverAdd));
 
    portNo = atoi(argv[1]);
 
+   //Address structure
    serverAdd.sin_family = AF_INET;
-   serverAdd.sin_addr.s_addr = INADDR_ANY;
+   serverAdd.sin_addr.s_addr = INADDR_ANY;  //any addr in the current host
    serverAdd.sin_port = htons(portNo); //convert values between host and network 
 
+   //bind addr and socket
    if(bind(sockFd, (struct sockaddr *) &serverAdd, sizeof(serverAdd)) < 0) {
         printf("Binding Error\n");
+        close(sockFd);
+        exit(0);
    }
 
-   listen(sockFd,5); // Arg 2: Number of Clients
+   listen(sockFd,1); // Arg 2: Number of Clients
+   printf("Waiting for incoming connection...\n");
+
    clientLen = sizeof(clientAdd);
 
-   clientSocketFd = accept(sockFd, (struct sockaddr *) &clientAdd, &clientLen);
+   //Accept incoming connection
+   clientSocketFd = accept(sockFd, (struct sockaddr *) &clientAdd,(socklen_t *)&clientLen);
 
    if(clientSocketFd < 0) {
-       printf("Error in creating Accept\n");
+       printf("Error in accepting connection\n");
+       close(clientSocketFd);
+       close(sockFd);
+       exit(0);
+   }
+   else {
+       printf("Connected to %s\nWelcome to the chatApp\n",inet_ntoa(clientAdd.sin_addr));
    }
 
+    do {
 
-   while(1) {
-    bzero(buffer,255);
-    n = read(clientSocketFd,buffer,255);
-    
-    if(n < 0) {
-            printf("Error reading from socket\n");
-     }
-    else {
-            printf("Message: %s\n",buffer);
-    }
+        bzero(rxBuffer,255);
+        if(recv(clientSocketFd,rxBuffer,255,0) < 0) {
+            printf("Error readingi from socket\n");
+            break;
+        }
+        printf("Message: %s\n",rxBuffer);
 
-    if(strncmp(buffer,"quitchat",8) == 0) {
-        close(sockFd);
-        close(clientSocketFd);
-        exit(0);
-    }
-    
-    bzero(txBuffer,255);
-    fgets(txBuffer,255,stdin);
-    n = write(clientSocketFd,txBuffer,strlen(txBuffer));
+        bye = strncasecmp(rxBuffer,"bye",3);
+        printf("bye: %d\n",bye);
+        if(bye == 0)
+            break;
 
-    if(n < 0) {
+        bzero(txBuffer,255);
+        printf("Enter message: ");
+        fgets(txBuffer,255,stdin);
+        n = send(clientSocketFd,txBuffer,strlen(txBuffer),0);
+        if(n < 0) {
             printf("Error writing to socket\n");
-    }
-   }
+            break;
+        }
+    
+        byebye =  strncasecmp(txBuffer,"bye",3);
+        printf("byebye: %d\n",byebye);
+        if(byebye == 0)
+            break;
+   } while(1); 
+   printf("Closing sockets\n");
+   close(clientSocketFd);
+   close(sockFd);
 
    return 0;
 }
